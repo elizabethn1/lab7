939f9b1a0a60f2bf2d73ab0e3bf7f7a2f67df90f
elizabethn1 <112528779+elizabethn1@users.noreply.github.com>
2022-11-08 08:55:03 -0600

    testing js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 function init(){
   function myEventFunction(){
-    var b = document.getElementById('entrybutton').value;
-    alert('Elizabeth Ng: ' + b);
+    var entry = document.getElementById('entrybutton').value;
+    alert('Elizabeth Ng: '+entry);
   }
   var button = document.getElementById('entrybutton');
   button.addEventListener('click', myEventFunction);
-
-
-
-
-
-
-
-
-
-  //var button = document.getElementById('entrybutton').name;
-  //button.addEventListener('click', myEventFunction);
 }
 window.addEventListener('load', init);
